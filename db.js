9a3543c53b43805532baefaa1c2a95cbc0951ac0
Asitsingh6394066616 <asitsingh0@gmail.com>
2023-09-11 12:25:32 +0530

    mongodb connect db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dbUri = 'mongodb+srv://asitsingh:Asitsingh18@cluster0.wpmlh2p.mongodb.net/?retryWrites=true&w=majority'
+const dbUri = 'mongodb+srv://asitsingh:ASc8P7i044jXYa8U@cluster3.ha4sqst.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.set('strictQuery', false)
 
