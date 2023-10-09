a10772c3dccd6b47e1a9dd3ff9e808f5aab5f468
Asitsingh6394066616 <asitsingh0@gmail.com>
2023-10-09 16:33:51 +0530

    TODO added

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+// TODO: - use SQLite3 for lite database
 const mongoose = require('mongoose')
 
 const dbUri = 'mongodb+srv://asitsingh:ASc8P7i044jXYa8U@cluster3.ha4sqst.mongodb.net/?retryWrites=true&w=majority'
